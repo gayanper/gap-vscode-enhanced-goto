0d892f09d80476d94485ec3d1c8abe3bc8c3dbe8
Gayan Perera <gayanper@gmail.com>
2025-01-20 18:54:06 +0100

    fix build

--- .github/workflows/cs.upload.sh
+++ .github/workflows/cs.upload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-NAME=$(echo $1 | awk -F"-" '{ print $1}')
+NAME=$(echo $1 | rev | cut -d'-' -f2- | rev)
 VERSION=$(echo $1 | awk -F"-" '{ print $NF}' | awk -F".vsix" '{ print $1}')-$2
 echo "## Uploading $1 with the version $VERSION ##"
-cloudsmith push raw gap/vsce $NAME-$VERSION.vsix --version $VERSION --republish
+
+PKG_NAME=$NAME-$VERSION.vsix
+mv $1 $PKG_NAME
+cloudsmith push raw gap/vsce $PKG_NAME --version $VERSION --republish
