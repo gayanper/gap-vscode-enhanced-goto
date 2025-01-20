b44085242284ad53abfb44e0b5021aaf5d43c5f5
Gayan Perera <gayanper@gmail.com>
2025-01-20 18:43:01 +0100

    make the file name unique for each build

--- .github/workflows/cs.upload.sh
+++ .github/workflows/cs.upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+NAME=$(echo $1 | awk -F"-" '{ print $1}')
 VERSION=$(echo $1 | awk -F"-" '{ print $NF}' | awk -F".vsix" '{ print $1}')-$2
 echo "## Uploading $1 with the version $VERSION ##"
-cloudsmith push raw gap/vsce $1 --version $VERSION --republish
+cloudsmith push raw gap/vsce $NAME-$VERSION.vsix --version $VERSION --republish
